87b61f08a910f4b5bc580b3dbf067c247641de3d
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-13 11:04:15 +0530

    data fetched using axios

--- src/components/card/AdviceCard.jsx
+++ src/components/card/AdviceCard.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const AdviceCard = () => {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.adviceslip.com/advice').then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, []);
+
   return <div></div>;
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import AdviceCard from '../../components/card/AdviceCard';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <AdviceCard />
+    </div>
+  );
 };
 
 export default Home;
